f09e4da99fa0c46d3fa7bb02a54c590b851c8453
André Vasconcelos <gp-andre.vasconcelos@uol.com.br>
2013-08-19 18:45:50 -0300

    Capitulo 01 - Atividade 04 - Testando onLoad, onCreate etc

--- OlaMundo/src/com/example/olamundo/OlaMundoActivity.java
+++ OlaMundo/src/com/example/olamundo/OlaMundoActivity.java
@@ -2,14 +2,16 @@ package com.example.olamundo;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
-public class OlaMundo extends Activity {
+public class OlaMundoActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.principal);
+        Log.i("Ciclo de vida", "onCreate");
     }
 
 
@@ -18,6 +20,18 @@ public class OlaMundo extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.ola_mundo, menu);
         return true;
+    }
+    
+    @Override
+    protected void onResume() {
+    	super.onResume();
+    	Log.i("Ciclo de vida", "onResume");
+    }
+    
+    @Override
+    protected void onDestroy() {
+    	super.onDestroy();
+    	Log.i("Ciclo de vida", "onDestroy");
     }
     
 }
