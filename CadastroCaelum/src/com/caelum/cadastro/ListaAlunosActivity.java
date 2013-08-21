19cd76f772fc084105214caa016daf78ef33af5d
André Vasconcelos <gp-andre.vasconcelos@uol.com.br>
2013-08-21 17:45:22 -0300

    Capitulo 01 - Atividade 03 - Tornando itens da lista clicaveis.

--- CadastroCaelum/src/com/caelum/cadastro/ListaAlunosActivity.java
+++ CadastroCaelum/src/com/caelum/cadastro/ListaAlunosActivity.java
@@ -2,25 +2,46 @@ package com.caelum.cadastro;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class ListaAlunosActivity extends Activity {
 
 	private ListView listaAlunos;
-	String[] alunos = { "Mimi", "Cocó", "Ranheta", "Lambari", "Havaiana jones",
-			"Feitoza", "Palhaço do satanás", "Fetinho do mal" };
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.listagem_alunos);
 
+		String[] alunos = { "Mimi", "Cocó", "Ranheta", "Tizil", "Lambari",
+				"Havaiana jones", "Feitoza", "Palhaço do satanás",
+				"Fetinho do mal" };
+		int layout = android.R.layout.simple_list_item_1;
+
 		listaAlunos = (ListView) findViewById(R.id.lista_alunos);
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1, alunos);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, layout,
+				alunos);
 
 		listaAlunos.setAdapter(adapter);
+
+		listaAlunos.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> adapter, View view,
+					int posicao, long id) {
+
+				Toast.makeText(ListaAlunosActivity.this,
+						"Posição do elemento " + (posicao + 1),
+						Toast.LENGTH_SHORT).show();
+
+			}
+
+		});
 
 	}
 
