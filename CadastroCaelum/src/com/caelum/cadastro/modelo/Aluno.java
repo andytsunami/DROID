0a5f96886b487340efa6d482823cb90cae95c5eb
André Vasconcelos <gp-andre.vasconcelos@uol.com.br>
2013-09-05 15:28:09 -0300

    Capitulo 04 - Atividade 01 - criando classe Aluno

---
+++ CadastroCaelum/src/com/caelum/cadastro/modelo/Aluno.java
@@ -0,0 +1,69 @@
+package com.caelum.cadastro.modelo;
+
+public class Aluno {
+	
+	private Long id;
+	private String nome;
+	private String endereço;
+	private String email;
+	private String site;
+	private String foto;
+	
+	@Override
+	public String toString() {
+		return nome;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereço() {
+		return endereço;
+	}
+
+	public void setEndereço(String endereço) {
+		this.endereço = endereço;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSite() {
+		return site;
+	}
+
+	public void setSite(String site) {
+		this.site = site;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	
+	
+	
+	
+
+}
