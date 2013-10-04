e4cb8f9056c0f68673fd42248103710c96b3f377
André Vasconcelos <gp-andre.vasconcelos@uol.com.br>
2013-10-04 10:59:36 -0300

    DROI2 - Capitulo 01- Atividade 02 - Implementando Serializable

--- CadastroCaelum/src/com/caelum/cadastro/modelo/Aluno.java
+++ CadastroCaelum/src/com/caelum/cadastro/modelo/Aluno.java
@@ -1,7 +1,13 @@
 package com.caelum.cadastro.modelo;
 
-public class Aluno {
+import java.io.Serializable;
 
+public class Aluno implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2462258682058164008L;
 	private Long id;
 	private String nome;
 	private String endereco;
