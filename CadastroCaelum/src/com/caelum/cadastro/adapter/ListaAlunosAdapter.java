8143051a032b151a908f3ebdafa26f9d412d69a0
André Vasconcelos <gp-andre.vasconcelos@uol.com.br>
2013-11-27 11:09:27 -0200

    DROI2 - Capitulo 03 - Atividade 02 - Criando ListaAlunosAdapter

---
+++ CadastroCaelum/src/com/caelum/cadastro/adapter/ListaAlunosAdapter.java
@@ -0,0 +1,49 @@
+package com.caelum.cadastro.adapter;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import com.caelum.cadastro.modelo.Aluno;
+
+public class ListaAlunosAdapter extends BaseAdapter{
+	
+	private final List<Aluno> alunos;
+	private final Activity activity;
+	
+	
+	public ListaAlunosAdapter(List<Aluno> alunos, Activity activity) {
+		
+		this.activity = activity;
+		this.alunos = alunos;
+		
+	}
+	
+	
+
+	@Override
+	public int getCount() {
+		return alunos.size();
+	}
+
+	@Override
+	public Object getItem(int posicao) {
+		
+		return alunos.get(posicao);
+	}
+
+	@Override
+	public long getItemId(int posicao) {
+		return alunos.get(posicao).getId();
+	}
+
+	@Override
+	public View getView(int arg0, View arg1, ViewGroup arg2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
