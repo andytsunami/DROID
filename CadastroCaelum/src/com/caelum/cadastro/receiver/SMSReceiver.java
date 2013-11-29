3cbdbe51bcd9f314038cc60c619e2f195a74e509
André Vasconcelos <gp-andre.vasconcelos@uol.com.br>
2013-11-29 11:22:42 -0200

    DROI2 - Capitulo 04 - Atividade 02 - Identificando origem do SMS

--- CadastroCaelum/src/com/caelum/cadastro/receiver/SMSReceiver.java
+++ CadastroCaelum/src/com/caelum/cadastro/receiver/SMSReceiver.java
@@ -3,13 +3,25 @@ package com.caelum.cadastro.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
 import android.widget.Toast;
 
 public class SMSReceiver extends BroadcastReceiver{
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		Toast.makeText(context, "Chegou um SMS champs!", Toast.LENGTH_LONG).show();
+		
+		Bundle bundle = intent.getExtras();
+		
+		Object mensagens[] = (Object[]) bundle.get("pdus");
+		
+		byte[] mensagem = (byte[]) mensagens[0];
+		
+		SmsMessage sms = SmsMessage.createFromPdu(mensagem);
+		
+		
+		Toast.makeText(context, "Chegou um SMS de: " + sms.getDisplayOriginatingAddress(), Toast.LENGTH_LONG).show();
 		
 	}
 	
