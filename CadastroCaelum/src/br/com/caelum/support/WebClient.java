c3d956b3af138cd4917fd0fb3dd1b10dd178fb7e
André Vasconcelos <gp-andre.vasconcelos@uol.com.br>
2014-01-07 15:17:52 -0200

    DROI3 - Capitulo 01 - Atividade 02 - Criendo WebClient

---
+++ CadastroCaelum/src/br/com/caelum/support/WebClient.java
@@ -0,0 +1,34 @@
+package br.com.caelum.support;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+public class WebClient {
+
+	private String url;
+
+	public WebClient(String url) {
+		this.url = url;
+	}
+
+	public String post(String json) throws Exception {
+		DefaultHttpClient httpClient = new DefaultHttpClient();
+
+		HttpPost post = new HttpPost(url);
+
+		post.setEntity(new StringEntity(json));
+
+		post.setHeader("Accept", "application/json");
+		post.setHeader("Content-type", "application/json");
+
+		HttpResponse response = httpClient.execute(post);
+		String jsonResposta = EntityUtils.toString(response.getEntity());
+
+		return jsonResposta;
+
+	}
+
+}
