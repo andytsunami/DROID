3656faa08ca747d1bc740de01975fa09e67a6cf6
André Vasconcelos <gp-andre.vasconcelos@uol.com.br>
2014-02-04 10:55:22 -0200

    DROI3 - Capitulo 04 - Atividade 04 - Atribuindo lista de provas infladas para para o atributo listViewProvas no Fragment

--- CadastroCaelum/src/br/com/caelum/fragment/ListaProvasFragment.java
+++ CadastroCaelum/src/br/com/caelum/fragment/ListaProvasFragment.java
@@ -5,16 +5,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.caelum.cadastro.R;
 
 public class ListaProvasFragment extends Fragment {
+	
+	private ListView listViewProvas;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View layoutProvas = inflater.inflate(R.layout.provas_lista, container,
 				false);
+		
+		this.listViewProvas = (ListView) layoutProvas.findViewById(R.layout.provas_lista);
 
 		return layoutProvas;
 	}
