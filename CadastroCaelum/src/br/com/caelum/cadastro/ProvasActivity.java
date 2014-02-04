13443c091f4b847694a60d77a1b208c7830fc657
André Vasconcelos <gp-andre.vasconcelos@uol.com.br>
2014-02-04 12:11:33 -0200

    DROI3 - Capitulo 04 - Atividade 08 - Criando activity de provas

---
+++ CadastroCaelum/src/br/com/caelum/cadastro/ProvasActivity.java
@@ -0,0 +1,15 @@
+package br.com.caelum.cadastro;
+
+import com.caelum.cadastro.R;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class ProvasActivity extends FragmentActivity{
+	@Override
+	protected void onCreate(Bundle bundle) {
+		super.onCreate(bundle);
+		setContentView(R.layout.provas);
+	}
+
+}
