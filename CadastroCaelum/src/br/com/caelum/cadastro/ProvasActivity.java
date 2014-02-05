5ee37af74ec064953496a0c7ce86bc1a69a51286
André Vasconcelos <gp-andre.vasconcelos@uol.com.br>
2014-02-05 16:30:47 -0200

    DROI3 - Capitulo 04 - Atividade 18 - Adicionando detalhes na lista de provas no metodo onCreate quando o device for com tela grande.

--- CadastroCaelum/src/br/com/caelum/cadastro/ProvasActivity.java
+++ CadastroCaelum/src/br/com/caelum/cadastro/ProvasActivity.java
@@ -1,5 +1,6 @@
 package br.com.caelum.cadastro;
 
+import br.com.caelum.fragment.DetalhesProvaFragment;
 import br.com.caelum.fragment.ListaProvasFragment;
 
 import com.caelum.cadastro.R;
@@ -8,20 +9,32 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 
-public class ProvasActivity extends FragmentActivity{
+public class ProvasActivity extends FragmentActivity {
 	@Override
 	protected void onCreate(Bundle bundle) {
 		super.onCreate(bundle);
 		setContentView(R.layout.provas);
-		
-		FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-		
-		transaction.replace(R.id.provas_view, new ListaProvasFragment(),"listaProvas");
-		
-		transaction.commit();
+		if (bundle == null) {
+
+			FragmentTransaction transaction = getSupportFragmentManager()
+					.beginTransaction();
+			if (isTablet()) {
+				transaction.replace(R.id.provas_lista,
+						new ListaProvasFragment(),
+						ListaProvasFragment.class.getCanonicalName()).replace(
+						R.id.provas_detalhe, new DetalhesProvaFragment(),
+						DetalhesProvaFragment.class.getCanonicalName());
+
+			} else {
+				transaction.replace(R.id.provas_view,
+						new ListaProvasFragment(), ListaProvasFragment.class.getCanonicalName());
+
+			}
+			transaction.commit();
+		}
 	}
-	
-	public boolean isTablet(){
+
+	public boolean isTablet() {
 		return getResources().getBoolean(R.bool.isTablet);
 	}
 
