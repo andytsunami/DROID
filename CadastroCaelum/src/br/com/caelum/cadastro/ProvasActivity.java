04f79b9b2d9e2dc198dd4adfd353dc117bef0ad0
André Vasconcelos <gp-andre.vasconcelos@uol.com.br>
2014-02-14 19:13:49 -0200

    DROI3 - capitulo 04 - Atividade 23 - permitindo o ussuario voltar para a listagem das provas ao clicar em voltar. E arrumado o bug do exercicio que não trazia os detalhes da prova em um tablet deitado.

--- CadastroCaelum/src/br/com/caelum/cadastro/ProvasActivity.java
+++ CadastroCaelum/src/br/com/caelum/cadastro/ProvasActivity.java
@@ -15,15 +15,17 @@ public class ProvasActivity extends FragmentActivity {
 	protected void onCreate(Bundle bundle) {
 		super.onCreate(bundle);
 		setContentView(R.layout.provas);
+		
+		FragmentTransaction transaction;
+		
 		if (bundle == null) {
-
-			FragmentTransaction transaction = getSupportFragmentManager()
+			transaction = getSupportFragmentManager()
 					.beginTransaction();
+			
 			if (isTablet()) {
-				transaction.replace(R.id.provas_lista,
-						new ListaProvasFragment(),
-						ListaProvasFragment.class.getCanonicalName()).replace(
-						R.id.provas_detalhe, new DetalhesProvaFragment(),
+				transaction.replace(R.id.provas_lista, new ListaProvasFragment(),
+						ListaProvasFragment.class.getCanonicalName())
+						.replace(R.id.provas_detalhe, new DetalhesProvaFragment(),
 						DetalhesProvaFragment.class.getCanonicalName());
 
 			} else {
@@ -40,16 +42,21 @@ public class ProvasActivity extends FragmentActivity {
 		return getResources().getBoolean(R.bool.isTablet);
 	}
 
-	public void selecionaProva(Prova selecionada) {
-		Bundle bundle = new Bundle();
-		bundle.putSerializable("prova", selecionada);
+	public void selecionaProva(Prova prova) {
+		Bundle argumentos = new Bundle();
+		argumentos.putSerializable("prova", prova);
 		DetalhesProvaFragment detalhesProva = new DetalhesProvaFragment();
-		detalhesProva.setArguments(bundle);
+		detalhesProva.setArguments(argumentos);
 
 		FragmentTransaction transaction = getSupportFragmentManager()
 				.beginTransaction();
+		
+		if(!isTablet()){
 		transaction.replace(R.id.provas_view, detalhesProva,
 				DetalhesProvaFragment.class.getCanonicalName());
+		} else {
+			transaction.replace(R.id.provas_detalhe, detalhesProva,DetalhesProvaFragment.class.getCanonicalName());
+		}
 		Toast.makeText(this, "Tablet? " + isTablet(), Toast.LENGTH_LONG).show();
 
 		if (!isTablet()) {
