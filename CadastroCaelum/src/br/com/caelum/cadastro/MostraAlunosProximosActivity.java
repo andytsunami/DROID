58f38d389a6150c460b948610a0d996c5336ccc6
André Vasconcelos <gp-andre.vasconcelos@uol.com.br>
2014-02-19 17:55:33 -0300

    DROI3 - Capitulo 05 - Atividade 11 - Implementando MostraAlunosProximosActivity

---
+++ CadastroCaelum/src/br/com/caelum/cadastro/MostraAlunosProximosActivity.java
@@ -0,0 +1,16 @@
+package br.com.caelum.cadastro;
+
+import com.caelum.cadastro.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MostraAlunosProximosActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.map_layout);
+	}
+
+}
