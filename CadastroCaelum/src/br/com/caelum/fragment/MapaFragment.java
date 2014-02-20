4604d0c2e4e845a81922329db9e662927f05d41c
André Vasconcelos <gp-andre.vasconcelos@uol.com.br>
2014-02-20 17:49:37 -0300

    DROI3 - Capitulo 05 - Atividade 17 - Localizando endereço no mapa e com zoom.

--- CadastroCaelum/src/br/com/caelum/fragment/MapaFragment.java
+++ CadastroCaelum/src/br/com/caelum/fragment/MapaFragment.java
@@ -1,21 +1,28 @@
 package br.com.caelum.fragment;
 
-import android.widget.Toast;
 import br.com.caelum.cadastro.util.Localizador;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 
-public class MapaFragment extends SupportMapFragment{
-	
+public class MapaFragment extends SupportMapFragment {
+
 	@Override
 	public void onResume() {
-		
+
 		super.onResume();
 		Localizador localizador = new Localizador(getActivity());
-		
-		LatLng coordenada = localizador.getCoordenada("Rua cesar Marengo 161 São Paulo");
-		Toast.makeText(getActivity(), "Coordenadas de casa: " + coordenada, Toast.LENGTH_LONG).show();
+
+		LatLng coordenada = localizador
+				.getCoordenada("Rua cesar Marengo 161 São Paulo");
+		this.localizaNo(coordenada);
+	}
+
+	private void localizaNo(LatLng coordenada) {
+		GoogleMap map = getMap();
+		map.moveCamera(CameraUpdateFactory.newLatLngZoom(coordenada,17));
 	}
 
 }
