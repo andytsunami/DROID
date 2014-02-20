aaf2d2b196dff5ce159d27f35a043b4024abcde3
André Vasconcelos <gp-andre.vasconcelos@uol.com.br>
2014-02-20 17:41:49 -0300

    DROI3 - capitulo 05 - Ativividade 16 - Lozalizando as coordenadas de casa.

--- CadastroCaelum/src/br/com/caelum/cadastro/util/Localizador.java
+++ CadastroCaelum/src/br/com/caelum/cadastro/util/Localizador.java
@@ -1,14 +1,39 @@
 package br.com.caelum.cadastro.util;
 
+import java.io.IOException;
+import java.util.List;
+
 import android.content.Context;
+import android.location.Address;
 import android.location.Geocoder;
+
+import com.google.android.gms.maps.model.LatLng;
 
 public class Localizador {
 
 	Geocoder geocoder;
+	Context context;
 
 	public Localizador(Context context) {
+		this.context = context;
 		geocoder = new Geocoder(context);
+	}
+
+	public LatLng getCoordenada(String endereco) {
+		try {
+			List<Address> listaEnderecos = geocoder.getFromLocationName(
+					endereco, 1);
+
+			if (!listaEnderecos.isEmpty()) {
+				Address address = listaEnderecos.get(0);
+				return new LatLng(address.getLatitude(), address.getLongitude());
+			} else {
+				return null;
+			}
+		} catch (IOException e) {
+			return null;
+		}
+
 	}
 
 }
--- CadastroCaelum/src/br/com/caelum/fragment/MapaFragment.java
+++ CadastroCaelum/src/br/com/caelum/fragment/MapaFragment.java
@@ -1,7 +1,21 @@
 package br.com.caelum.fragment;
 
+import android.widget.Toast;
+import br.com.caelum.cadastro.util.Localizador;
+
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
 public class MapaFragment extends SupportMapFragment{
+	
+	@Override
+	public void onResume() {
+		
+		super.onResume();
+		Localizador localizador = new Localizador(getActivity());
+		
+		LatLng coordenada = localizador.getCoordenada("Rua cesar Marengo 161 São Paulo");
+		Toast.makeText(getActivity(), "Coordenadas de casa: " + coordenada, Toast.LENGTH_LONG).show();
+	}
 
 }
