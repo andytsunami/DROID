0a04635c754fcd7f61da9e4260095f7ac6200c07
André Vasconcelos <gp-andre.vasconcelos@uol.com.br>
2014-02-20 18:02:39 -0300

    DROI3 - Capitulo 05 - Atividade 18 - Mostrando alunos proximos a meu endereço.

--- CadastroCaelum/src/br/com/caelum/fragment/MapaFragment.java
+++ CadastroCaelum/src/br/com/caelum/fragment/MapaFragment.java
@@ -1,28 +1,49 @@
 package br.com.caelum.fragment;
 
+import java.util.List;
+
+import br.com.caelum.cadastro.dao.AlunoDAO;
+import br.com.caelum.cadastro.modelo.Aluno;
 import br.com.caelum.cadastro.util.Localizador;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapaFragment extends SupportMapFragment {
 
 	@Override
 	public void onResume() {
-
 		super.onResume();
-		Localizador localizador = new Localizador(getActivity());
+		Localizador coderUtil = new Localizador(getActivity());
+		LatLng local = coderUtil
+				.getCoordenada("Rua cesar marengo 161 São Paulo");
+		this.localizaNo(local);
 
-		LatLng coordenada = localizador
-				.getCoordenada("Rua cesar Marengo 161 São Paulo");
-		this.localizaNo(coordenada);
+		AlunoDAO alunoDAO = new AlunoDAO(getActivity());
+		List<Aluno> alunos = alunoDAO.getLista();
+		alunoDAO.fecha();
+
+		for (Aluno aluno : alunos) {
+			Localizador localizador = new Localizador(getActivity());
+			LatLng coordenada = localizador.getCoordenada(aluno.getEndereco());
+
+			if (coordenada != null) {
+				MarkerOptions markerOptions = new MarkerOptions();
+				markerOptions.position(coordenada).title(aluno.getNome())
+						.snippet(aluno.getEndereco());
+
+			}
+
+		}
+
 	}
 
 	private void localizaNo(LatLng coordenada) {
 		GoogleMap map = getMap();
-		map.moveCamera(CameraUpdateFactory.newLatLngZoom(coordenada,17));
+		map.moveCamera(CameraUpdateFactory.newLatLngZoom(coordenada, 17));
 	}
 
 }
