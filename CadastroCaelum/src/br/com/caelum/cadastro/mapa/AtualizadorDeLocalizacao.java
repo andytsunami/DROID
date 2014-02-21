b7c3e6fe43baddd21dff61dcea2efac667398a50
André Vasconcelos <gp-andre.vasconcelos@uol.com.br>
2014-02-21 11:46:19 -0300

    DROI3 - Capitulo 06 - Atividade 01 - Criando Atualizador de localização e implementando LocationListener

---
+++ CadastroCaelum/src/br/com/caelum/cadastro/mapa/AtualizadorDeLocalizacao.java
@@ -0,0 +1,36 @@
+package br.com.caelum.cadastro.mapa;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class AtualizadorDeLocalizacao implements LocationListener {
+
+	@Override
+	public void onLocationChanged(Location location) {
+		LatLng latLng = new LatLng(location.getLongitude(),
+				location.getLatitude());
+
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
